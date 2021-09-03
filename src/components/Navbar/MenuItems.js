f19102d378c5bb528c47335a2de41e944a3d4837
unknown <Leo hengebaert>
2021-09-03 15:28:54 +0200

    navbar react

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,30 @@
+
+
+export const MenuItems = [
+    {
+        title: 'Home',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Services',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Produits',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Contact',
+        url: '#',
+        cName: 'nav-links'
+    },
+    {
+        title: 'Sign up',
+        url: '#',
+        cName: 'nav-links-mobile'
+    },
+]
+
